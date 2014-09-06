3143b4d41cb3b4c3cf7f6fde36b1ee254efe697e
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-09-06 20:32:03 +0200

    Deleting framework dir for now. Moving code from framework bootstrap to lib bootstrap

--- web/lib/bootstrap.php
+++ web/lib/bootstrap.php
@@ -12,5 +12,46 @@
 // Exit if accessed directly.
 if (!defined('ABSPATH')) exit;
 
+/**
+ * Load files in directories.
+ *
+ * @param string $lib_root
+ */
+
+function isopress_load_files ($lib_root) {
+  $libs_dir = @opendir($lib_root);
+  $lib_files = array();
+
+  while (($file = readdir($libs_dir)) !== false) {
+    if (substr($file, 0, 1) == '.' ) {
+      continue;
+    }
+
+    if (is_dir($lib_root . '/' . $file)) {
+      $base_path = $lib_root . '/' . $file;
+
+      // Load libraries, will search for this files and require them:
+      // - directory/directory.php
+      // - directory/lib/directory.php
+
+      if (is_file($base_path . '/' . $file . '.php')) {
+        $lib_files[] = $base_path . '/' . $file . '.php';
+      } else if (is_file($base_path . '/lib/' . $file . '.php')) {
+        $lib_files[] = $base_path . '/lib/' . $file . '.php';
+      }
+    }
+  }
+
+  foreach ($lib_files as $lib_file) {
+    require_once $lib_file;
+  }
+}
+
 // Load all directories in this directory.
 isopress_load_files(dirname(__FILE__));
+
+function test_load () {
+  register_ptb_directory(dirname(__FILE__) . '/page-types');
+}
+
+add_action('plugins_loaded', 'test_load');
--- web/wp-config.php
+++ web/wp-config.php
@@ -7,5 +7,4 @@
 
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 require_once(dirname(__DIR__) . '/config/application.php');
-require_once(dirname(__DIR__) . '/framework/bootstrap.php');
 require_once(ABSPATH . 'wp-settings.php');
